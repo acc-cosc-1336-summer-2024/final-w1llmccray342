c5ea2d30fd2930363c30e64c31045a18366c6e3c
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-22 15:20:24 +0000

    fix test_cases

--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -2,13 +2,10 @@
 import unittest
 
 #follow this example to add questions b, c, and d for testing including their functions
-from question_a.question_a import test_config
-from question_c.question_c import grab_lowest_nums, grab_highest_nums, grab_avg_nums, grab_total_nums
+
+from question_c import grab_lowest_nums, grab_highest_nums, grab_avg_nums, grab_total_nums
 
 class Test_Config(unittest.TestCase):
-
-    def test_question_a_config(self):
-        self.assertEqual(True, test_config())
 
     def test_question_c_grab_lowest_nums(self):
         nums_to_test = [1, 2, 3, 4, 5]
