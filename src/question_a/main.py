d50adb8c748c80ef41d0f256b7d9922300f6d7c5
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-22 14:03:11 +0000

    fix an incorrect import statement

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,7 +1,7 @@
-from question_a.question_a import Stock
+from question_a import question_a
 
 # Create a basic stock.
-goog_stock = Stock("GOOG", "Google")
+goog_stock = question_a.Stock("GOOG", "Google")
 
 # Print type of GOOG_Stock to ensure it is of "STOCK" type class
 print(goog_stock)
