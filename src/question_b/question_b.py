6e4085e54337b40db5414fedb7eaf90ba836e16a
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-22 14:23:03 +0000

    Setup initial commit for Question_b

--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,17 @@
-#write functions here, don't add input('') statements here!
+class Stock:
+    
+    # Create a constructor with symbol and company params.
+    def __init__(self, symbol, company_name):
+        self.__symbol = symbol
+        self.__company_name = company_name
+
+    # Create a public method that returns the symbol
+    def return_symbol(self):
+        return self.__symbol
+
+    # Create a public method that returns the company_name:
+    def return_name(self):
+        return self.__company_name
+        
+    def stock_purchase_history(self):
+        pass
