221b269704d55cb520e75c67fd2336d5bb0baa92
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-22 15:19:39 +0000

    setup test functions for question C

--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -1,1 +1,44 @@
 #write functions here, don't add input('') statements here!
+
+# Create a function that asks the user to enter a series of 5 numbers
+def get_user_data():
+    my_list = []
+
+
+    for i in range(5):
+        my_temp_num = int(input("Please enter 5 integers you would like to check \n"))
+        my_list.append(my_temp_num)
+
+    return my_list
+
+# Create a function that grabs the LOWEST number in a list
+def grab_lowest_nums(list):
+    list.sort()
+    return list[0]
+
+
+
+# Create a function that grabs the HIGHEST number in a list
+def grab_highest_nums(list):
+    list.sort()
+    return list[-1]
+
+# Create a function that grabs the total numbers in a list
+def grab_total_nums(list):
+    sum = 0
+
+    for nums in list:
+        sum += nums
+
+    return sum
+
+# Create a function that grabs the AVERAGE numbers in a list
+def grab_avg_nums(sum, list):
+    total_nums = 0 
+    
+    for nums in list:
+        total_nums += 1
+
+    average = sum / total_nums
+
+    return average
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -3,10 +3,19 @@ import unittest
 
 #follow this example to add questions b, c, and d for testing including their functions
 from question_a.question_a import test_config
+from question_c.question_c import grab_lowest_nums, grab_highest_nums, grab_avg_nums, grab_total_nums
 
 class Test_Config(unittest.TestCase):
 
     def test_question_a_config(self):
         self.assertEqual(True, test_config())
+
+    def test_question_c_grab_lowest_nums(self):
+        nums_to_test = [1, 2, 3, 4, 5]
+        self.assertEqual(1, grab_lowest_nums(nums_to_test))
+
+    def test_question_c_grab_highest_nums(self):
+        nums_to_test = [1, 2, 3, 4, 5]
+        self.assertEqual(5, grab_highest_nums(nums_to_test))
 
 
