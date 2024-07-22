7920b6ddd0b6268b21c9084908661e7c2cc6cddf
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-22 14:37:44 +0000

    Implemented Stock functions in main

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,24 @@
-#add import
+from question_b import Stock
+
+still_running = True  
+
+def main_menu():
+    print("1. Display stock purchase history")
+    print("2. Exit")
+
+def handle_menu_options():
+    main_menu()
+
+    while option != 2:
+        option = input("Please select what you would like to do\n")
+
+        if option == 1:
+            Stock.stock_purchase_history()
+    
+
+def main():
+    handle_menu_options()
+
+if __name__ == "__main__":
+    main()
+  
