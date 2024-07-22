2232d58c8b1d4f7c09e39005fa5f2ca0b8abf90d
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-22 14:32:24 +0000

    Create a stock_purchase_history using lists, loops, and dictionaries.

--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -14,4 +14,24 @@ class Stock:
         return self.__company_name
         
     def stock_purchase_history(self):
-        pass
+        # Create several pre-defined stocks
+        apple_stk = Stock("AAPL", "Apple Inc")
+        cate_stk = Stock("CAT", "Caterpillar")
+        ek_stk = Stock("EK", "Eastman Kodak")
+        google_stk = Stock("GOOG", "Google")
+        msft_stk = Stock("MSFT", "Microsoft")
+
+        # Add stocks to a temporary list
+        temp_stocks = [apple_stk, cate_stk, ek_stk, google_stk, msft_stk]
+
+        # Initialize an empty dictionary with my_stocks
+        my_stocks = {}
+        
+        for stock in temp_stocks:
+            my_stock_symbol = stock.return_symbol
+            my_stock_name = stock.return_name
+
+            my_stocks.update[my_stock_symbol][my_stock_name]
+        
+        for key, value in my_stocks.items():
+            print (key, value)
