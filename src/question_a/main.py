96cee3f973daf8ce4ce6be1e478d8e5eb5fe71c7
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-22 14:01:34 +0000

    Some debug to check main functions

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,4 +1,8 @@
 from question_a import Stock
 
+# Create a basic stock.
 goog_stock = Stock("GOOG", "Google")
+
+# Print type of GOOG_Stock to ensure it is of "STOCK" type class
+print(type(goog_stock))
 print(goog_stock)
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,9 +1,12 @@
 #write functions here, don't add input('') statements here!
 class Stock:
     
+    # Create a constructor with symbol and company params.
     def __init__(self, symbol, company_name):
         self.__symbol = symbol
         self.__company_name = company_name
 
+    # Create a method to return the symbol and company name, in this case we can just use the builtin __STR__ method to achieve
+    # What we are trying to accomplish
     def __str__(self):
         return f"Stock Report \n{self.__symbol} \n{self.__company_name} "
