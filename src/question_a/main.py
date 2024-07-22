17d9eb19fbf8a64e405ecab0fd60e8cef7c566f8
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-22 14:11:56 +0000

    Check type of goog_stock

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,6 +1,7 @@
-from stocks_a import Stock
+from question_a import Stock
 # Create a basic stock.
 goog_stock = Stock("GOOG", "Google")
 
 # Print type of GOOG_Stock to ensure it is of "STOCK" type class
+print(type(goog_stock))
 print(goog_stock)
